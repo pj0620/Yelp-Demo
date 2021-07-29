3b0e3c271c01b50bc3be71eeae0fe34add23c5b1
pj0620 <pjm7748@g.rit.edu>
2021-07-28 22:41:59 -0400

    added property to switch between api/webscrapper for retreving review data

--- src/main/java/com/yelp/yelp/demo/spring/ServiceConfig.java
+++ src/main/java/com/yelp/yelp/demo/spring/ServiceConfig.java
@@ -1,5 +1,6 @@
 package com.yelp.yelp.demo.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,11 +10,20 @@ import com.yelp.yelp.demo.services.WebScrapperYelpServiceImpl;
 import com.yelp.yelp.demo.services.YelpService;
 
 @Configuration
-@PropertySource("classpath:yelp-api.properties")
+@PropertySource("classpath:yelp.properties")
 public class ServiceConfig {
+
 	@Bean
-	public YelpService yelpService() {
-		return new WebScrapperYelpServiceImpl();
-//		return new APIYelpServiceImpl();
+	public YelpService yelpService(
+			@Value("${review.retrieve.method}") String retrieveMethod
+	) {
+		switch (retrieveMethod) {
+			case "webScrapper":
+				return new WebScrapperYelpServiceImpl();
+			case "api":
+				return new APIYelpServiceImpl();
+			default:
+				return new APIYelpServiceImpl();
+		}
 	}
 }
